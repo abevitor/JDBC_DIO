0de533b3554bd8145c802ddecf4279b49d814bf3
abevitor <blueprates86@gmail.com>
2025-08-11 14:50:14 -0300

    Inicio

---
+++ demo/demo/src/main/java/com/example/demo/persistence/ConnectionUtil.java
@@ -0,0 +1,23 @@
+package com.example.demo.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ConnectionUtil {
+
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(
+    "jdbc:mysql://localhost:3306/jdbc_sample?useSSL=false&serverTimezone=UTC&characterEncoding=UTF-8",
+    "root",
+    "Vitriz3003!"
+);    
+    }
+    
+}
+
